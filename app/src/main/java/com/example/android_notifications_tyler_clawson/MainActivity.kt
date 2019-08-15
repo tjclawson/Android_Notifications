a7ccff2668647d219af42eccaf3dc88b401c6ad7
tjclawson <tjamesclawson@gmail.com>
2019-08-15 15:16:52 -0400

    starting assignment

---
+++ app/src/main/java/com/example/android_notifications_tyler_clawson/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_notifications_tyler_clawson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
